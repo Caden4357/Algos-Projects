7dd4c02b6668bcf92eb36fd90ed2b880eb691181
Caden Wilcox <wilcox4357@gmail.com>
2022-06-05 13:29:50 -0700

    couple more simple string algo prep for tmrw

--- strings/stringsOne.js
+++ strings/stringsOne.js
@@ -29,7 +29,7 @@ console.log(getDigits("abc8c0d1ngd0j0!8"))
 const acronym = (str) => {
     let output = ""
     output += str[0].toUpperCase()
-    console.log(output)
+    // console.log(output)
     for (var i = 1; i < str.length; i++){
         if (str[i+1] == " "){
             output += str[i+2].toUpperCase()
@@ -38,3 +38,29 @@ const acronym = (str) => {
     return output
 }
 console.log(acronym("there's no free lunch - gotta pay yer way."))
+console.log(acronym("Live from New York, it's Saturday Night!"))
+
+// Create a function that, given a string, returns the number of non-space characters found in the string. 
+const countNonSpaces = (str) => {
+    let count = 0
+    for (var i = 0; i < str.length; i++){
+        if (str[i] != " "){
+            count += 1
+        }
+    }return count 
+}
+console.log(countNonSpaces("Honey   pie, you are driving me   crazy   "))
+console.log(countNonSpaces("Hello world !"))
+
+// Create a function that, given an array of strings and a numerical value, returns an array that only contains strings longer than or equal to the given value.
+
+const removeShortStrings = (arr, val) => {
+    let output = []
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i].length >= val){
+            output.push(arr[i])
+        }
+    }return output
+}
+console.log(removeShortStrings(['Good morning', 'sunshine', 'the', 'Earth', 'says', 'hello'], 4))
+console.log(removeShortStrings(['There', 'is', 'a', 'bug', 'in', 'the', 'system'], 3))
