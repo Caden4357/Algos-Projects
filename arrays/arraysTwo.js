60d3cef7148a40a1568ebc3f8feb35c5fc839c6b
Caden Wilcox <wilcox4357@gmail.com>
2022-06-05 00:40:09 -0700

    couple algos still need work with edge cases

--- arrays/arraysTwo.js
+++ arrays/arraysTwo.js
@@ -1,12 +1,17 @@
 // Given a numerical array, reverse the order of values, in-place. The reversed array should have the same length, with existing elements moved to other indices so that order of elements is reversed. Working 'in-place' means that you cannot use a second 
 // array – move values within the array that you are given. As always, do not use built-in array functions such as splice().
 const reverse = (arr) => {
-    console.log(arr.length/2)
+    if (arr.length < 2){
+        return arr;
+    }
     for (var i = 0; i < arr.length/2; i++){
         let temp = arr[i]
         arr[i] = arr[arr.length-1-i]
         arr[arr.length-1-i] = temp
     }
-    return arr
+    return arr;
 }
 console.log(reverse([1,2,3,4,5]))
+console.log(reverse([1,2]))
+console.log(reverse([100]))
+console.log(reverse([-90, 700, 800, -2, 0.435, 9045, 0, -70000, 80]))
--- strings/stringsOne.js
+++ strings/stringsOne.js
@@ -10,3 +10,31 @@ const removeBlanks = (str) => {
 }
 removeBlanks(" Pl ayTha tF u nkyM usi c ")
 removeBlanks("I can not BELIEVE it's not BUTTER")
+
+
+// Create a JavaScript function that given a string, returns the integer made from the string’s digits. You are allowed to use isNaN() and Number().
+
+const getDigits = (str) => {
+    let output = ""
+    for (var i = 0; i < str.length; i++){
+        if (!isNaN(str[i])){
+            output += str[i]
+        }
+    }return output
+}
+console.log(getDigits("abc8c0d1ngd0j0!8"))
+
+// Create a function that, given a string, returns the string’s acronym (first letter of the word capitalized). You are allowed to use .split() and .toUpperCase().
+// ! THIS NEEDS WORK WITH MANY EDGE CASES 
+const acronym = (str) => {
+    let output = ""
+    output += str[0].toUpperCase()
+    console.log(output)
+    for (var i = 1; i < str.length; i++){
+        if (str[i+1] == " "){
+            output += str[i+2].toUpperCase()
+        }
+    }
+    return output
+}
+console.log(acronym("there's no free lunch - gotta pay yer way."))
